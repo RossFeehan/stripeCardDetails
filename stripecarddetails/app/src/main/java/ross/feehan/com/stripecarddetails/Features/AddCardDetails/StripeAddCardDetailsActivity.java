2fe93a84431164a3e72c60036e6ae27f835881f2
rossfeehan <thafirstboss@yahoo.co.uk>
2016-05-11 15:36:38 +0100

    Created display card details xml layout

--- stripecarddetails/app/src/main/java/ross/feehan/com/stripecarddetails/Features/AddCardDetails/StripeAddCardDetailsActivity.java
+++ stripecarddetails/app/src/main/java/ross/feehan/com/stripecarddetails/Features/AddCardDetails/StripeAddCardDetailsActivity.java
@@ -1,16 +1,11 @@
 package ross.feehan.com.stripecarddetails.Features.AddCardDetails;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -52,7 +47,7 @@ public class StripeAddCardDetailsActivity extends AppCompatActivity implements A
     }
 
     private void setupUI() {
-        setContentView(R.layout.stripe_add_card_details_layout);
+        setContentView(R.layout.add_card_details_layout);
         ButterKnife.bind(this);
         setUpToolbar();
         cardNumberET.addTextChangedListener(addTextWatcher());
