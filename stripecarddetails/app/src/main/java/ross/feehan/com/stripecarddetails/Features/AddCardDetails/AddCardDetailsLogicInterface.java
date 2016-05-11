775980f9ef643313b1c1396ffca3537bf7d03f42
rossfeehan <thafirstboss@yahoo.co.uk>
2016-05-11 12:12:29 +0100

    Added interface methods to add card logic and view interfaces

--- stripecarddetails/app/src/main/java/ross/feehan/com/stripecarddetails/Features/AddCardDetails/AddCardDetailsLogicInterface.java
+++ stripecarddetails/app/src/main/java/ross/feehan/com/stripecarddetails/Features/AddCardDetails/AddCardDetailsLogicInterface.java
@@ -3,4 +3,37 @@ package ross.feehan.com.stripecarddetails.Features.AddCardDetails; /*
  */
 
 public interface AddCardDetailsLogicInterface {
+
+    public static final int FIRST_AMERICAN_EXPRESS_PREFIX = 34;
+    public static final int SECOND_AMERICAN_EXPRESS_PREFIX = 37;
+    public static final int VISA_PREFIX = 4;
+    public static final int MASTERCARD_START_PREFIX = 51;
+    public static final int MASTERCARD_END_PREFIX = 55;
+    public static final int UNKNOWN_CARD_PREFIX_LENGTH = 4;
+
+    public static final String AMERICAN_EXPRESS = "American Express";
+    public static final String MASTERCARD = "Master Card";
+    public static final String VISA = "Visa";
+    public static final String UNKNOWN_CARD = "Unknown";
+
+    /**
+     * Find the card type for the entered card number
+     * @param cardNumber
+     */
+    void getCardType(int cardNumber);
+
+    /**
+     * Called when the save button is clicked
+     */
+    void onSaveButtonClicked();
+
+    /**
+     * receive the users card details
+     * @param cardholderName
+     * @param cardNumber
+     * @param expMonth
+     * @param expYear
+     * @param cvv
+     */
+    void receiveCardDetails(String cardholderName, String cardNumber, String expMonth, String expYear, String cvv);
 }
--- stripecarddetails/app/src/main/java/ross/feehan/com/stripecarddetails/Features/AddCardDetails/AddCardDetailsViewInterface.java
+++ stripecarddetails/app/src/main/java/ross/feehan/com/stripecarddetails/Features/AddCardDetails/AddCardDetailsViewInterface.java
@@ -3,4 +3,27 @@ package ross.feehan.com.stripecarddetails.Features.AddCardDetails; /*
  */
 
 public interface AddCardDetailsViewInterface {
+
+    /**
+     * Display the card type to the user
+     * @param cardType
+     */
+    void displayCardType(String cardType);
+
+    /**
+     * Fetch the card details from the edit texts
+     */
+    void getCardDetails();
+
+    /**
+     * Display message to user
+     * @param message
+     */
+    void displayMessage(String message);
+
+    /**
+     * Called when the card has successfully been saved
+     * @param message
+     */
+    void displayMessageAndCloseScreen(String message);
 }
