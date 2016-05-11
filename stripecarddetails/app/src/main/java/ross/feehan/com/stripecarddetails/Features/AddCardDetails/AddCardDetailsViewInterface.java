d5db68f6f159a9490b5a59308e0b12a282b2578e
rossfeehan <thafirstboss@yahoo.co.uk>
2016-05-11 12:10:53 +0100

    Created ui for add card details

--- stripecarddetails/app/src/main/java/ross/feehan/com/stripecarddetails/Features/AddCardDetails/AddCardDetailsViewInterface.java
+++ stripecarddetails/app/src/main/java/ross/feehan/com/stripecarddetails/Features/AddCardDetails/AddCardDetailsViewInterface.java
@@ -2,5 +2,5 @@ package ross.feehan.com.stripecarddetails.Features.AddCardDetails; /*
  * Created by Ross Feehan on 11/05/2016.
  */
 
-public class AddCardDetailsViewInterface {
+public interface AddCardDetailsViewInterface {
 }
